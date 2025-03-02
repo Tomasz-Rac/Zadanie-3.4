660a4e8bebe3784162b5e9a07289df9660c7d85f
Tomasz Raczy <tomaszraczy@gmail.com.>
2025-03-02 18:32:52 +0100

    Add main1.py

--- main1.py
+++ main1.py
@@ -6,4 +6,4 @@ sets_sum=sum(len(wartosci) for wartosci in zakupy.values())
 for nazwa, (wartosc) in zakupy.items():
     print(f"Idę do {nazwa}, kupiuję tu następujące rzeczy:{wartosc}")
 
-#Hej Patryk!
+# Hej Partyk!
